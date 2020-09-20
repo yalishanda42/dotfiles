4e1dddac0af7550b57ff6a73f19568ea951e603a
Alexander Ignatov <yalishanda@abv.bg>
2020-09-20 20:30:35 +0300

    Add tests

---
+++ tests/tests.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function cleanup() {
+    rm -rf ~/.oh-my-zsh
+    rm -rf ~/.vimrc
+}
+
+cleanup
+
+echo Installing without sudo and without vim plugins...
+../INSTALL.sh < <(echo nn) || exit 1
+
+cleanup
+
+echo Installing without sudo and wit vim plugins...
+../INSTALL.sh < <(echo ny) || exit 1
+
+cleanup
+
+echo Updating...
+../UPDATE.sh || exit 1
+
+
+echo All tests OK
+exit 0
