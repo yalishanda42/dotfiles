f4bb4788aa4246d693eef714766b924f5c484142
Alexander Ignatov <yalishanda@abv.bg>
2020-09-20 20:40:48 +0300

    cd of tests

--- tests.sh
+++ tests.sh
@@ -8,17 +8,17 @@ function cleanup() {
 cleanup
 
 echo Installing without sudo and without vim plugins...
-../INSTALL.sh < <(echo nn) || exit 1
+./INSTALL.sh < <(echo nn) || exit 1
 
 cleanup
 
 echo Installing without sudo and wit vim plugins...
-../INSTALL.sh < <(echo ny) || exit 1
+./INSTALL.sh < <(echo ny) || exit 1
 
 cleanup
 
 echo Updating...
-../UPDATE.sh || exit 1
+./UPDATE.sh || exit 1
 
 
 echo All tests OK
