e2bef9531e439cb02c8aaf824abbca73b476aacf
Alexander Ignatov <yalishanda@abv.bg>
2021-08-19 00:00:42 +0300

    Update update script

--- UPDATE.sh
+++ UPDATE.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
 
-function __git_prompt_git () {
-	GIT_OPTIONAL_LOCKS=0 command git "$@"
-}
-
-function current_branch () {
-    local ref
-	ref=$(__git_prompt_git symbolic-ref --quiet HEAD 2> /dev/null)
-	local ret=$?
-	if [[ $ret != 0 ]]
-	then
-		[[ $ret == 128 ]] && return
-		ref=$(__git_prompt_git rev-parse --short HEAD 2> /dev/null)  || return
-	fi
-	echo ${ref#refs/heads/}
-}
-
-git pull --rebase origin $(current_branch) && rm -rf ~/.oh-my-zsh && ./INSTALL.sh < <(echo nn)
+git checkout master && git pull origin master && rm -rf ${ZSH} && ./INSTALL.sh < <(echo nn)
