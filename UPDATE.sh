a39868d5f8d9be66db733adc6559163167fa941b
Alexander Ignatov <yalishanda@abv.bg>
2021-08-19 00:13:04 +0300

    Make update script failable

--- UPDATE.sh
+++ UPDATE.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-git checkout master && git pull origin master && rm -rf ${ZSH} && ./INSTALL.sh < <(echo nn)
+fail() {
+	echo "Update failed. $*"
+	exit 1
+}
+
+git checkout master || fail "Discard any changes in this directory and try again."
+git pull origin master || fail
+rm -rf ${ZSH} || fail "Make sure you have necessary permission for ${ZSH}"
+unset ZSH
+./INSTALL.sh < <(echo nn) || fail
