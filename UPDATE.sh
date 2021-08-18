cf9c7ce3b62a1850dbd77b15e5212359a44bae3b
Alexander Ignatov <yalishanda@abv.bg>
2021-08-19 00:28:29 +0300

    Use zsh command and set vimrc too

--- UPDATE.sh
+++ UPDATE.sh
@@ -9,4 +9,4 @@ git checkout master || fail "Discard any changes in this directory and try again
 git pull origin master || fail
 rm -rf ${ZSH} || fail "Make sure you have necessary permission for ${ZSH}"
 unset ZSH
-./INSTALL.sh < <(echo nn) || fail
+./INSTALL.sh < <(echo ny) || fail
